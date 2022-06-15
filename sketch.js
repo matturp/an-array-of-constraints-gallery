caacf16d9d064b78e11ed75196bccddb20f6ae46
matturp <81508090+matturp@users.noreply.github.com>
2022-06-15 21:38:55 +0100

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,14 @@
+
+db.collection("images").get().then((querySnapshot) => {
+  querySnapshot.forEach((doc) => {
+      // doc.data() is never undefined for query doc snapshots
+      console.log(doc.id, " => ", doc.data());
+
+      const image = document.createElement('img');
+image.setAttribute('src', doc.data().image);
+image.setAttribute('width', 450 + 'px');
+document.getElementById('gallery').appendChild(image);
+  });
+});
+
+
